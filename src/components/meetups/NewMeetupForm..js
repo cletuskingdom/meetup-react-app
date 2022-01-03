840239d4931628f12a5697378e51544ab641d7ad
Cletus Kingdom <cletuskingdom@gmail.com>
2022-01-04 00:19:24 +0100

    users can submit form data to firebase.

--- src/components/meetups/NewMeetupForm..js
+++ src/components/meetups/NewMeetupForm..js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -24,6 +24,7 @@ function NewMeetupForm() {
     };
 
     // console.log(meetupData); submit this to db
+    props.onAddMeetup(meetupData);
   }
 
   return (
