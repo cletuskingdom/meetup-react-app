c0ec172eb3039b17fd16b135289dd8eca12f4d0f
Cletus Kingdom <cletuskingdom@gmail.com>
2022-01-04 01:51:43 +0100

    users can add and remove favourite using states.

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavouritesContext from "../store/favourite-content";
+
 function FavouritesPage() {
-  return <div>Favourite page</div>;
+  const favouristeCtx = useContext(FavouritesContext);
+
+  let content;
+
+  if (favouristeCtx.totalFavourites === 0) {
+    content = <p>You got no favourites yet, start adding some.</p>;
+  } else {
+    content = <MeetupList meetups={favouristeCtx.favourites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavouritesPage;
